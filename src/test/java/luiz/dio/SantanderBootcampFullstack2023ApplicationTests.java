68208cdb470db869599e3c95c8076a80cd13e711
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-10-20 18:32:38 -0300

    Primeiro commit

---
+++ src/test/java/luiz/dio/SantanderBootcampFullstack2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package luiz.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampFullstack2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
